1de811e13b09e3d80c788c47aada2b9f4c9cfbd5
FabAstorga06 <astorgafabian6@gmail.com>
2018-04-17 00:50:40 -0600

    Actualiza progra

--- procesador_imagenes/constants.h
+++ procesador_imagenes/constants.h
@@ -6,11 +6,10 @@
 #include <highgui.h>
 
 /* Constantes */
-#define PARAMS 5
+#define PARAMS 3
 #define SIZE_W 200
 #define TITLE_W "Image Converter"
 #define BIT_AMOUNT 8
-#define OK 1
 
 /* Funciones para encriptar */
 #define PIX_VEC 4
@@ -23,10 +22,6 @@ static uint8_t private_key = 0x0FA; // 250 en decimal
 static uint8_t bits_displ = 2; // desplazamiento
 static uint8_t pix_arr[PIX_VEC] = {12, 5, 100, 10}; //Clave de pixeles
 
-/* Parametro de entrada que determina el algoritmo de encriptacion */
-static int num_encrypt;
-
 /* Dimensiones y canales de la imagen */
 unsigned int img_h = 0;
 unsigned int img_w = 0;
-unsigned int img_channels = 0;
---
+++ procesador_imagenes/main.cpp
@@ -0,0 +1,100 @@
+#include "encrypt.cpp"
+#include "exceptions.cpp"
+
+/*********************************************************************************/
+
+void convert2gray(char* img_name, cv::Mat &img_proc ) {
+
+  // Carga una imagen
+  img_proc = cv::imread(img_name, CV_LOAD_IMAGE_GRAYSCALE );
+  // Verifica si la imagen se cargo exitosamente
+  if(!img_proc.data ) {
+    printf("No fue posible cargar la imagen: %s\n",img_name);
+    exit(0);
+  }
+
+  img_h = img_proc.rows;
+  img_w = img_proc.cols;
+
+  // Crea ventana para mostrar imagen
+  cv::namedWindow(TITLE_W, cv::WINDOW_AUTOSIZE);
+  cv::moveWindow(TITLE_W, img_w, img_h);
+
+}
+
+void show_image (cv::Mat &img) {
+  // Muestra la imagen
+  cv::imshow(TITLE_W, img );
+  // Espera por una señal del teclado para cerrar ventana
+  cv::waitKey(0);
+}
+
+/*********************************************************************************/
+
+int main(int argc, char* argv[] )  {
+
+  /* Maneja excepciones de los parámetros de entrada */
+  int num_encrypt = atoi(argv[2]);
+  if (argc != PARAMS) {
+    printf("Inserte los parámetros correspondientes...\n");
+    exit(0);
+  }
+  if(num_encrypt < 1 || num_encrypt > 4 ) {
+    printf("Inserte los parámetros correspondientes...\n");
+    exit(0);
+  }
+
+  /*******************************************************************/
+
+  /* SE MUESTRA IMAGEN EN ESCALA DE GRISES */
+
+  /* Se convierte imagen a escala de grises */
+  cv::Mat img_g;
+  convert2gray(argv[1], img_g );
+  show_image(img_g);
+
+  /*******************************************************************/
+  /* SE ENCRIPTA IMAGEN */
+  switch(num_encrypt) {
+
+     case 1:
+        /* Encriptacion con XOR */
+        printf("----ENCRYPT XOR----\n");
+        xor_encrypt(img_g);
+        /* Desencripta con la misma funcion XOR */
+        printf("----DECRYPT XOR----\n");
+        xor_encrypt(img_g);
+        break;
+
+     case 2:
+       /* Encriptacion con desplazamiento simple */
+        printf("----ENCRYPT SIMPLE DISPLACE----\n");
+        simple_desp(img_g);
+       /* Desencripta con desplazamiento simple (se pierde info) */
+        printf("----DECRYPT SIMPLE DISPLACE----\n");
+        decrypt_simple_desp(img_g);
+        break;
+     case 3:
+        /* Encriptacion con desplazamiento circular */
+        printf("----ENCRYPT CIRCULAR DISPLACE----\n");
+        circular_desp(img_g);
+        /* Desencripta con desplazamiento circular  */
+        printf("----DECRYPT CIRCULAR DISPLACE----\n");
+        decrypt_circular_desp(img_g);
+        break;
+
+     case 4:
+     /* Encriptacion con suma simple */
+        printf("----ENCRYPT SIMPLE ADD----\n");
+        simple_add(img_g);
+        /* desencripta con suma simple */
+        printf("----DECRYPT SIMPLE ADD----\n");
+        simple_add_decrypt(img_g);
+        break;
+
+      default:
+         break;
+  }
+
+  return 0;
+}
